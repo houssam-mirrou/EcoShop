e786c4da739b70480938fa5e664cf82dfe553795
houssam-mirrou <hossam.mirrou@gmail.com>
2026-03-29 02:30:03 +0100

    fixed the routing system

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\CheckoutController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,7 +12,39 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+
+    Route::middleware('admin')->group(function () {
+        // Gestion des Produits
+        Route::post('/products', [ProductController::class, 'store']);
+        Route::put('/products/{product}', [ProductController::class, 'update']);
+        Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
+        // Gestion des Catégories
+        Route::post('/categories', [CategoryController::class, 'store']);
+        Route::put('/categories/{category}', [CategoryController::class, 'update']);
+        Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+
+        // Consultation de toutes les commandes
+        Route::get('/orders', [OrderController::class, 'index']);
+    });
+
+    Route::middleware('user')->group(function () {
+        // Gestion du Panier
+        Route::get('/cart', [CartController::class, 'index']);
+        Route::post('/cart', [CartController::class, 'store']);
+        Route::put('/cart/{product}', [CartController::class, 'update']);
+        Route::delete('/cart/{product}', [CartController::class, 'destroy']);
+
+        // Passer la commande
+        Route::post('/checkout', [CheckoutController::class, 'store']);
+    });
 });
